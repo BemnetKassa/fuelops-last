f91153bad839ccd0554cb542d5f063655f1fc4c7
Bemnet kassa Bekele <bemnetkassa79@gmail.com>
2026-01-23 16:34:46 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ import adminRoutes from './src/routes/adminRoutes.js';
 import stationAdminRoutes from './src/routes/stationAdminRoutes.js';
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 // Enable CORS for all routes
 app.use(cors());
