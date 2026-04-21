ec83931537e944994b0324768386b5505a3572f9
Bemnet kassa Bekele <bemnetkassa79@gmail.com>
2026-04-21 16:50:07 +0300

    old prisma configuration fixing
    my prisma was pooling configuration was a bit old not compatible for version v7 so updated the prisma client code by installing and adding prisma adapter to the pooling

--- backend/src/db/prisma.js
+++ backend/src/db/prisma.js
@@ -1,21 +1,24 @@
-  import pkg from '@prisma/client';
-  const { PrismaClient } = pkg;
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
 
-  // PrismaClient is attached to the `global` object in development to prevent
-  // exhausting your database connection limit.
-  //
-  // Learn more: https://pris.ly/d/help/next-js-best-practices
+const globalForPrisma = globalThis;
 
-  const globalForPrisma = global;
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
 
-  const prisma =
-    globalForPrisma.prisma ||
-    new PrismaClient({
-      log: ['query'],
-    });
+const adapter = new PrismaPg(pool);
 
-  if (process.env.NODE_ENV !== 'production') {
-    globalForPrisma.prisma = prisma;
-  }
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+    log: process.env.NODE_ENV === 'development' ? ['query'] : [],
+  });
 
-  export default prisma;
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
+}
+
+export default prisma;
